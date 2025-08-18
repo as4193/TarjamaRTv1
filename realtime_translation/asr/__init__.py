320c9b1f236c7a6c3668749889f88ed6837539c6
Abdelaziz Saud <abdel_aziz419@outlook.com>
2025-08-18 20:26:34 +0400

    Refactor ASR module interface
    Add language mapping utilities and remove ASRManager from public exports for cleaner module structure.

--- realtime_translation/asr/__init__.py
+++ realtime_translation/asr/__init__.py
@@ -1,11 +1,13 @@
-from .asr_engine import ASREngine, ASRResult, ASRSegment, ASRManager
+from .asr_engine import ASREngine, ASRResult, ASRSegment
 from .whisper_ct2 import WhisperCT2Model, create_whisper_asr
+from .language_mapping import map_language_to_iso, map_language_to_full
 
 __all__ = [
     'ASREngine',
     'ASRResult',
     'ASRSegment', 
-    'ASRManager',
     'WhisperCT2Model',
-    'create_whisper_asr'
+    'create_whisper_asr',
+    'map_language_to_iso',
+    'map_language_to_full'
 ] 
